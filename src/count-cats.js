333584c7a7fb11b6121d3ec63a955ff8552bcf38
Andryteck <772316andrew@gmail.com>
2020-02-17 17:05:48 +0300

    feat:function cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,12 +1,12 @@
 module.exports = function countCats(backyard) {
-  let cat = 0;
-  for (let i = 0; i < catsMatrix.length; i++) {
-      for (let j = 0; j < catsMatrix[i].length; j++) {
-          if (catsMatrix[i][j] == "^^") {
-              cat++;
+  let cats = 0;
+  for (let i = 0; i < backyard.length; i++) {
+      for (let j = 0; j < backyard[i].length; j++) {
+          if (backyard[i][j] == "^^") {
+              cats++;
           }
 
       }
   }
-  return cat;
+  return cats;
 };
