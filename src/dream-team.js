a0a39dbecfc2a0d84c4a76a3828ae2d0c6ecc36d
Andryteck <772316andrew@gmail.com>
2020-02-19 14:20:02 +0300

    feat: func

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,8 @@
-function createDreamTeam(members) {
+module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) return false;
   let dreamTeam = '';
-  for (let memberStr of members) {
-    if (typeof memberStr == "string") {
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
       dreamTeam += members[i].split(' ').join('')[0];
     }
   }
