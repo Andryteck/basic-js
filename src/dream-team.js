0c030b7e947d4c6172c6a85f384c5d845235e88b
Andryteck <772316andrew@gmail.com>
2020-02-19 13:52:47 +0300

    feat: func

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let dreamTeam = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == "string") {
+      dreamTeam += members[i].split(' ').join('')[0];
+    }
+  }
+  dreamTeam = dreamTeam.toUpperCase().split('').sort().join('');
+  return dreamTeam;
+ 
 };
+return Array.isArray(members);
+
+
+
