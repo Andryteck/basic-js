50955456a7456978f68e5cc45094d89364761510
Andryteck <772316andrew@gmail.com>
2020-03-04 21:42:51 +0300

    feat:implement func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw Error;
+    }
+
+
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case "--discard-next":
+                i++;
+                break;
+            case '--discard-prev':
+                result.pop();
+            case "--double-next":
+                result.push(i++);
+            case "--double-prev":
+                result.pop(i++);
+            default:
+                result.push(arr[i]);
+                break;
+        }
+    }
+    return result;
 };
+
